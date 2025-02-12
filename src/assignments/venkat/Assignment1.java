3f94b336dd608a557eb84a5d96227aaab1ed67f8
Venkat3991 <cvramireddy1993@gmail.com>
2025-02-12 13:38:48 +0530

    Update Assignment1.java

--- src/assignments/venkat/Assignment1.java
+++ src/assignments/venkat/Assignment1.java
@@ -3,8 +3,23 @@ package assignments.venkat;
 public class Assignment1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int num = 5; 
+       
+        if (isPrime(num)) {
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
+        }
+    }
 
-	}
-
+    static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
 }
+		
+
+	
